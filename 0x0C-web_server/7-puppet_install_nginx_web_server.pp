abbddf51959ff78be446dac4ef3fe2177fc036fc
Drew <andrew.maring@gmail.com>
2019-08-08 21:25:11 +0000

    Add 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+package {'nginx':
+  ensure => installed,
+}
+
+file {'/var/www/html/index.html':
+    content => 'Holberton School',
+}
+
+file_line { 'title':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.espn.com permanent;',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
