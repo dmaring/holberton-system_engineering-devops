ee3d69c922487797621d6a935edee950e6036be6
Drew <andrew.maring@gmail.com>
2019-08-16 22:14:45 +0000

    Remove unnecessary lines in 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,18 +3,7 @@ package {'nginx':
   ensure  => installed,
 }
 
-file {'/var/www/html/index.html':
-    content => 'Holberton School',
-}
-
-file_line { 'title':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _',
-  line   => 'rewrite ^/redirect_me https://www.espn.com permanent;',
-}
-
-file_line { 'add header':
+file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
