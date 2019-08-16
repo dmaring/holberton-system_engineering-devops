6f7df54660f2092c497c816818ee5accf5e0c300
Drew <andrew.maring@gmail.com>
2019-08-16 21:52:09 +0000

    Add 2-puppet_custom_http_response-header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,27 @@
+# puppet file that installs and configures nginx
+package {'nginx':
+  ensure => installed,
+}
+
+file {'/var/www/html/index.html':
+    content => 'Holberton School',
+}
+
+file_line { 'title':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.espn.com permanent;',
+}
+
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'add_header X-Served-By $HOSTNAME;',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
