c66a9ebb6379bb1cc13ef3b0fb8a99f96d493bca
Drew <andrew.maring@gmail.com>
2019-09-16 17:43:31 +0000

    Add 2-export_to_JSON.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""A python script that returns information about an
+employees TODO list progress.
+"""
+import csv
+import json
+import requests
+import sys
+
+
+def get_todo_info():
+    """A function that gets the todo information for a particular user id"""
+    user_id = sys.argv[1]
+    # GET /user/<id> resource for user info
+    r = requests.get('https://jsonplaceholder.typicode.com/users?id={}'
+                     .format(user_id))
+    user = json.loads(r.text)
+    user_name = user[0]['username']
+
+    # GET /user/<id>/todos for todo info
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                     .format(user_id))
+    todos = json.loads(r.text)
+
+    with open ('USER_ID.csv', 'w', newline='', encoding='utf-8') as fp:
+        taskwriter = csv.writer(fp, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            taskwriter.writerow(["{}".format(user_id),
+                                 "{}".format(user_name),
+                                 "{}".format(task['completed']),
+                                 "{}".format(task['title'])])
+
+
+if __name__ == "__main__":
+    get_todo_info()
