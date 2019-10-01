9e16066a6755471f5266f2c5b9aeecd2d84d73b8
Drew <andrew.maring@gmail.com>
2019-10-01 23:45:03 +0000

    Add 0-strace_is_your_friend.pp 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+file { '/var/www/html/wp-settings.php':
+  ensure => present,
+}
+-> exec { 'sed file':
+  command  => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/\' wp-settings.php; apache2ctl restart',
+  cwd      => '/var/www/html',
+  path     => ['/bin/', '/usr/sbin/'],
+}
