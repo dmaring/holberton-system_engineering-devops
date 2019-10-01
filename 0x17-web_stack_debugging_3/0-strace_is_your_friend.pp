098b3fdf6af26be328484af8b970fd4ecd20125a
Drew <andrew.maring@gmail.com>
2019-10-01 23:47:49 +0000

    Modify for pupper-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@ file { '/var/www/html/wp-settings.php':
   ensure => present,
 }
 -> exec { 'sed file':
-  command  => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/\' wp-settings.php; apache2ctl restart',
-  cwd      => '/var/www/html',
-  path     => ['/bin/', '/usr/sbin/'],
+  command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/\' wp-settings.php; apache2ctl restart',
+  cwd     => '/var/www/html',
+  path    => ['/bin/', '/usr/sbin/'],
 }
