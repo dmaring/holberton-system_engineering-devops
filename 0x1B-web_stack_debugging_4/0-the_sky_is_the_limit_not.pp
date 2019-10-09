66a683e556fd19c28db8cf4196f6328d5173b063
Drew <andrew.maring@gmail.com>
2019-10-09 22:36:22 +0000

    Use service nginx restart instead of nginx -s reload

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,6 @@
 # This puppet manifest increases the number of workers for nginx
-file { '/etc/nginx/nginx.conf':
-  ensure => present,
-}
--> exec { 'sed file':
-  command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"\' /etc/default/nginx; nginx -s reload',
+exec { 'sed file':
+  command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"\' /etc/default/nginx; service nginx restart',
   cwd     => '/root',
   path    => ['/bin', '/usr/sbin'],
 }
