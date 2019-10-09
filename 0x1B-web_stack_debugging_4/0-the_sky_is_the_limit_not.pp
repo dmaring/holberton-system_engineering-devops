dc0a70f3c1f13685c1edcd3f1a8b01e4a788be4f
Drew <andrew.maring@gmail.com>
2019-10-09 20:50:53 +0000

    Add 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,9 @@
-#!/usr/bin/env bash
+# This puppet manifest increases the number of workers for nginx
+file { '/etc/nginx/nginx.conf':
+  ensure => present,
+}
+-> exec { 'sed file':
+  command => 'sed -i \'s/worker_processes 4/worker_processes 12/\' /etc/nginx/nginx.conf; nginx -s reload',
+  cwd     => '/root',
+  path    => ['/bin', '/usr/sbin'],
+}
