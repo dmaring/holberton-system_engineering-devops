e6c86140c40daab96557e0008070ccb6dd9f3172
Drew <andrew.maring@gmail.com>
2019-10-09 22:19:18 +0000

    Edit 0-the_sky_is_the_limit_not.pp to change ULIMIT on nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@ file { '/etc/nginx/nginx.conf':
   ensure => present,
 }
 -> exec { 'sed file':
-  command => 'sed -i \'s/worker_processes 4/worker_processes 12/\' /etc/nginx/nginx.conf; nginx -s reload',
+  command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"\' /etc/default/nginx; nginx -s reload',
   cwd     => '/root',
   path    => ['/bin', '/usr/sbin'],
 }
