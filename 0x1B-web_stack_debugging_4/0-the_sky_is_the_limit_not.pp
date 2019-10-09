1869ea80cde4f02fb9ae05899d6f04cfefbc1ca2
Drew <andrew.maring@gmail.com>
2019-10-09 23:04:35 +0000

    Rearrange quotes for puppet exec

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # This puppet manifest changes the ULIMIT for nginx
 exec { 'sed file':
-  command => 'sed -i \'s/15/4096\' /etc/default/nginx; service nginx restart',
+  command => "sed -i 's/15/4096/g' /etc/default/nginx; service nginx restart",
   path    => ['/bin', '/usr/sbin', '/usr/bin'],
 }
