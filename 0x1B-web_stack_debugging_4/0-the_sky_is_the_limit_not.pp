7fde118b1be0e32fd6c67bf740c3b620384ca1e2
Drew <andrew.maring@gmail.com>
2019-10-09 22:41:05 +0000

    Remove unnecessary lines

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # This puppet manifest increases the number of workers for nginx
 exec { 'sed file':
   command => 'sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"\' /etc/default/nginx; service nginx restart',
-  cwd     => '/root',
-  path    => ['/bin', '/usr/sbin'],
+  path    => ['/bin', '/usr/sbin', '/usr/bin'],
 }
